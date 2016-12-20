971ff7b6c04d2af44b12f5a24561990ecef28243
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2016-12-20 12:52:23 +0100

    probando matplotlib

--- trabajo.py
+++ trabajo.py
@@ -1,4 +1,13 @@
-import sys
-k= sys.version
+import matplotlib.pyplot as plt
+import numpy as np
 
-print ("hola mundo")
+t = np.arange(0.0, 2.0, 0.01)
+s = np.sin(2*np.pi*t)
+plt.plot(t, s)
+
+plt.xlabel('time (s)')
+plt.ylabel('voltage (mV)')
+plt.title('About as simple as it gets, folks')
+plt.grid(True)
+plt.savefig("test.png")
+plt.show()
